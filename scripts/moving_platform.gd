fd2e8b1c6a985269a0cde8dc6ce4de965247b57a
rulaicd-lining <lining@laibot.cn>
2024-12-11 17:09:16 +0800

    feature: first commit

---
+++ scripts/moving_platform.gd
@@ -0,0 +1,27 @@
+extends StaticBody2D
+
+
+enum Type { SPRING, AUTOMN, WINTER, SUMMER }
+
+@onready var sprite = $Sprite2D
+
+@export var type: Type = Type.SPRING
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	setSpriteRange()
+
+func setSpriteRange():
+	match type:
+		Type.SPRING:
+			sprite.region_rect = Rect2(16, 0, 32, 9)
+		Type.AUTOMN:
+			sprite.region_rect = Rect2(16, 32, 32, 9)
+		Type.WINTER:
+			sprite.region_rect = Rect2(16, 96, 32, 9)
+		_:
+			pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
---
+++ scripts/player.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+
+const SPEED = 150.0
+const JUMP_VELOCITY = -350.0
+
+@onready var animte2d := $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction < 0:
+		animte2d.flip_h = true
+	elif direction > 0:
+		animte2d.flip_h = false
+
+	if is_on_floor():
+		if direction:
+			animte2d.play("run")
+		else:
+			animte2d.play("idle")
+	else:
+		animte2d.play("jump")
+
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
