9eca627cfd13ff320690c6eda11efc292237820d
littleGauze <810483824@qq.com>
2024-12-13 00:01:00 +0800

    feature: add mistery bricks

---
+++ scripts/coin_physics.gd
@@ -0,0 +1,10 @@
+extends RigidBody2D
+
+func _on_body_entered(_body: Node2D):
+	pass
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("Coin entered area ", body.name)
+	%GameManager.add_score(1)
+	$AnimationPlayer.play('pickup')
---
+++ scripts/mistery_brick.gd
@@ -0,0 +1,21 @@
+extends StaticBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print('mistery brick ready')
+	pass # Replace with function body.
+
+
+func _on_area_2d_body_entered(body:Node2D) -> void:
+	hit()
+
+func hit():
+	var up: Tween = get_tree().create_tween()
+	up.tween_property(self, 'position', Vector2(position.x, position.y - 2), .1)
+	up.set_trans(Tween.TRANS_CUBIC)
+	up.set_ease(Tween.EASE_OUT)
+	var down: Tween = up.chain()
+	down.tween_property(self, 'position', Vector2(position.x, position.y), .1)
+	down.set_trans(Tween.TRANS_CUBIC)
+	down.set_ease(Tween.EASE_OUT)
